1b3f50ffd58ad0ced5356ab41fd1371e143068ab
Ahsan <rasul.ahsan@gmail.com>
2021-07-20 12:11:01 +0000

    Only outputs names shorter than 12 characters

---
+++ directory.2.rb
@@ -0,0 +1,43 @@
+# get user input
+def input_students
+  puts "please enter the name of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get student name
+  name = gets.chomp
+  # while the name is not empty, repeat the following
+  while !name.empty? do
+    # add student hash to array
+    students <<  {name: name, cohort: :november}
+    # get another name from user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+  
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+# prints student name with index only if shorter than 12 characters
+def print(students)
+  students.each_with_index do |student, index|
+    if student[:name].length < 12
+      puts " #{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end 
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+#here we call the methods
+students = input_students
+print_header
+print(students)
+print_footer(students)
